8cd5186b91316c044459f76515f6363745208b19
Chris Price <chris@puppetlabs.com>
2016-07-20 17:16:20 -0700

    WIP: fix typo

--- site/profile/manifests/sysop/packages/cronie.pp
+++ site/profile/manifests/sysop/packages/cronie.pp
@@ -1,5 +1,5 @@
 class profile::sysop::packages::cronie(
-    $package_name = $profile::sysops::packages::cronie::params::package_name
+    $package_name = $profile::sysop::packages::cronie::paramspackage_name
 ) inherits profile::sysop::packages::cronie::params {
   package { $package_name : ensure => latest; }
 }
