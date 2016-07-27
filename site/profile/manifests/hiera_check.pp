86420aee554591f8b449455d2a7b445f2adb951e
Chris Price <chris@puppetlabs.com>
2016-07-27 13:42:03 -0700

    WIP: testing hiera calls

--- site/profile/manifests/hiera_check.pp
+++ site/profile/manifests/hiera_check.pp
@@ -24,7 +24,7 @@ class profile::hiera_check(
     fail("Hiera lookup appears to have failed; location_stage_checkfield: '${location_stage_checkfield}'")
   }
 
-  $group_checkfield = hiera('group::pgtomcat::hiera_check_field')
+  $group_checkfield = hiera('groups::pgtomcat::hiera_check_field')
   if ($group_checkfield != "correction value") {
     fail("Hiera lookup appears to have failed; group_checkfield: '${$group_checkfield}'")
   }
