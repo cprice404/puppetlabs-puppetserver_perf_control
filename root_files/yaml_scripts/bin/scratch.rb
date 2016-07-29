37320aa872a06993dabee3cf188ce4305e3f5b23
Chris Price <chris@puppetlabs.com>
2016-07-28 20:24:08 -0700

    WIP: working on groups/group/location/stage

--- root_files/yaml_scripts/bin/scratch.rb
+++ root_files/yaml_scripts/bin/scratch.rb
@@ -1,19 +1,17 @@
 #!/usr/bin/env ruby
 
 require File.join(File.dirname(__FILE__), "..", "lib", "yaml_maker.rb")
+require 'fileutils'
 
 root_dir = File.join(File.dirname(__FILE__), "..", "..", "..")
 
 ym = YamlMaker.new
 
-(1..45).each do |i|
-  d = File.join(root_dir, "hieradata/groups/group#{i}")
-  if ! Dir.exists?(d) then Dir.mkdir(d) end
-  stage = "test"
-  if (rand(8) == 0)
-    f = File.join(root_dir, "hieradata/groups/group#{i}/#{stage}.yaml")
-    m = (rand(20) <= 3) ?
-        ym.make_big_map("groups::group#{i}::#{stage}::big_map", 1 + rand(5)) : ""
-    File.write(f, "---\n" + m + "\n" + ym.make_uuids(0, "groups::group#{i}::#{stage}::uuid", 3 + rand(10)))
+(1..3).each do |i|
+  d = File.join(root_dir, "hieradata/groups/group#{i}/loc1")
+  if ! Dir.exists?(d) then FileUtils.mkdir_p(d) end
+  ["dev", "prod", "stage"].each do |stage|
+    f = File.join(root_dir, "hieradata/groups/group#{i}/loc1/#{stage}.yaml")
+      File.write(f, "---\n" + ym.make_uuids(0, "groups::group#{i}::loc1::#{stage}::uuid", 4))
   end
 end
