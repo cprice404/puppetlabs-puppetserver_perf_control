6b4625015d81e5872a27ea59e187c61ac569eafe
Chris Price <chris@puppetlabs.com>
2016-07-29 13:21:13 -0700

    WIP: working on node level

--- root_files/yaml_scripts/bin/scratch.rb
+++ root_files/yaml_scripts/bin/scratch.rb
@@ -7,72 +7,20 @@ root_dir = File.join(File.dirname(__FILE__), "..", "..", "..")
 
 ym = YamlMaker.new
 
-# (1..3).each do |i|
-#   d = File.join(root_dir, "hieradata/groups/group#{i}/loc1")
-#   if ! Dir.exists?(d) then FileUtils.mkdir_p(d) end
-#   ["dev", "prod", "stage"].each do |stage|
-#     f = File.join(root_dir, "hieradata/groups/group#{i}/loc1/#{stage}.yaml")
-#     File.write(f, "---\n" + ym.make_uuids(0, "groups::group#{i}::loc1::#{stage}::uuid", 4))
-#   end
-# end
+(1..400).each do |i|
+  domain = 1 + rand(6)
+  domain_name = "subdomain#{domain}.mycompany.org"
+  host = "host#{i}.#{domain_name}"
+  d = File.join(root_dir, "hieradata/nodes/#{domain_name}")
+  FileUtils.mkdir_p(d)
+  f = File.join(d, "#{host}.yaml")
+  uuids = ym.make_uuids(0, "nodes::subdomain#{domain}_mycompany_org::host#{i}_subdomain#{domain}_mycompany_org::uuid",
+                        2 + rand(15))
+  m = ""
 
-# d = File.join(root_dir, "hieradata/groups/group4/loc2")
-# FileUtils.mkdir_p(d)
-# ["prod", "stage"].each do |stage|
-#   f = File.join(d, "#{stage}.yaml")
-#   File.write(f, "---\n" + ym.make_uuids(0, "groups::group4::loc2::#{stage}::uuid", 50) +
-#       "\n" + ym.make_big_map("groups::group4::loc2::#{stage}::big_map", 5))
-# end
-
-# (1..75).each do |i|
-#   group = 1 + rand(45)
-#   function = 1 + rand(20)
-#   d = File.join(root_dir, "hieradata/groups/group#{group}")
-#   FileUtils.mkdir_p(d)
-#   f = File.join(d, "function#{function}.yaml")
-#   uuids = ym.make_uuids(0, "groups::group#{group}::function#{function}::uuid",
-#                         1 + rand(6))
-#   m = ""
-#
-#   if i < 4
-#     m = ym.make_big_map("groups::group#{group}::function#{function}::big_map", 10)
-#   elsif i < 15
-#     uuids += "\n" + ym.make_uuids(0, "groups::group#{group}::function#{function}::moaruuid",
-#                                   8 + rand(5))
-#   end
-#
-#   File.write(f, "---\n" + uuids + "\n" + m)
-#
-# end
-
-# (1..20).each do |i|
-#   group = 1 + rand(45)
-#   function = 1 + rand(20)
-#   stage = ["dev", "test", "prod", "stage"][rand(4)]
-#   d = File.join(root_dir, "hieradata/groups/group#{group}/function#{function}")
-#   FileUtils.mkdir_p(d)
-#   f = File.join(d, "#{stage}.yaml")
-#   uuids = ym.make_uuids(0, "groups::group#{group}::function#{function}::#{stage}::uuid",
-#                         1 + rand(2))
-#   m = ""
-#
-#   if i < 2
-#     m = ym.make_big_map("groups::group#{group}::function#{function}::#{stage}::big_map", 5)
-#   end
-#
-#   File.write(f, "---\n" + uuids + "\n" + m)
-#
-# end
-
-["test"].each do |stage|
-  d = File.join(root_dir, "hieradata/domains/subdomain2.mycompany.org/stages")
-  f = File.join(d, "#{stage}.yaml")
-  uuids = ym.make_uuids(0, "domains::subdomain2_mycompany_org::stages::#{stage}::uuid",
-                        10)
-  m = ym.make_big_map("domains::subdomain2_mycompany_org::stages::#{stage}::bigmap1", 4) +
-      "\n" +
-      ym.make_big_map("domains::subdomain2_mycompany_org::stages::#{stage}::bigmap2", 4)
-
+  if i < 15
+    m = ym.make_big_map("nodes::subdomain#{domain}_mycompany_org::host#{i}_subdomain#{domain}_mycompany_org::uuid", 2 + rand(8))
+  end
 
   File.write(f, "---\n" + uuids + "\n" + m)
 end
